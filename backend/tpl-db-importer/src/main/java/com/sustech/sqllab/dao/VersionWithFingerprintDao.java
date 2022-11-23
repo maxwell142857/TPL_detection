c749fb47e4531bc7a796f8e7c825cf3db37d7a23
WLQ <836200779@qq.com>
2022-11-23 17:18:24 +0800

    Add `VersionWithFingerprintDao`

---
+++ backend/tpl-db-importer/src/main/java/com/sustech/sqllab/dao/VersionWithFingerprintDao.java
@@ -0,0 +1,7 @@
+package com.sustech.sqllab.dao;
+
+import com.github.yulichang.base.MPJBaseMapper;
+import com.sustech.sqllab.po.VersionWithFingerprint;
+
+public interface VersionWithFingerprintDao extends MPJBaseMapper<VersionWithFingerprint> {
+}

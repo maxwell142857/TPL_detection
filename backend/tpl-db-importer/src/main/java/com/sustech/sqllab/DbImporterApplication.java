d33110f3050d793b3300b8ab33b97367eff13f3d
WLQ <836200779@qq.com>
2022-11-23 21:03:10 +0800

    Complete DbImporter's framework

--- backend/tpl-db-importer/src/main/java/com/sustech/sqllab/DbImporterApplication.java
+++ backend/tpl-db-importer/src/main/java/com/sustech/sqllab/DbImporterApplication.java
@@ -1,9 +1,13 @@
 package com.sustech.sqllab;
 
+import com.sustech.sqllab.dao.ArtifactDao;
 import com.sustech.sqllab.dao.FingerprintDao;
+import com.sustech.sqllab.dao.GroupDao;
 import com.sustech.sqllab.dao.VersionDao;
-import com.sustech.sqllab.po.Fingerprint;
+import com.sustech.sqllab.po.Artifact;
+import com.sustech.sqllab.po.Group;
 import com.sustech.sqllab.po.Version;
+import lombok.SneakyThrows;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -15,12 +19,10 @@ import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashSet;
 
 @SpringBootApplication
 @MapperScan("com.sustech.sqllab.dao")
-public class DbImporterApplication /*implements ApplicationRunner */{
+public class DbImporterApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbImporterApplication.class, args);
@@ -31,22 +33,32 @@ public class DbImporterApplication /*implements ApplicationRunner */{
 	@Resource
 	private VersionDao versionDao;
 	@Resource
+	private ArtifactDao artifactDao;
+	@Resource
+	private GroupDao groupDao;
+	@Resource
 	private FingerprintDao fingerprintDao;
-//	@Override
-	public void run(ApplicationArguments args) throws IOException {
-		//noinspection ConstantConditions
-		for (File versionFile : new File(rootPath).listFiles()) {
-			String fileName = versionFile.getName();
-			String name = fileName.substring(0, fileName.lastIndexOf('.'));
-			System.out.println("Start "+name);
-			Version version =Version.builder()
-									.artifactId(0)
-									.usage(0)
-									.name(name)
-									.build();
-			versionDao.insert(version);
-			for (String hash : new HashSet<>(Files.readAllLines(Path.of(versionFile.getAbsolutePath())))) {
-				fingerprintDao.insert(new Fingerprint(hash,version.getId()));
+
+	@SuppressWarnings("ConstantConditions")
+	@SneakyThrows(IOException.class)
+	@Override
+	public void run(ApplicationArguments args){
+		for (File groupDir : new File(rootPath).listFiles()) {
+			Group group = new Group(null, groupDir.getName());
+			groupDao.insert(group);
+			for (File artifactDir : groupDir.listFiles()) {
+				Artifact artifact = new Artifact()
+									   .setGroupId(group.getId())
+									   .setName(artifactDir.getName());
+				artifactDao.insert(artifact);
+				for (File versionFile : artifactDir.listFiles()) {
+					String versionName = versionFile.getName();
+					Version version =new Version()
+										.setArtifactId(artifact.getId())
+										.setName(versionName);
+					versionDao.insert(version);
+					Files.readAllLines(versionFile.toPath());
+				}
 			}
 		}
 	}

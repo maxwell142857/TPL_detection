99e2ce93e2cffcc6e27b42fc2c88c1c6b40336fa
WLQ <836200779@qq.com>
2023-01-03 19:53:01 +0800

    Analyze multiple apk

--- backend/tpl-db-importer/src/test/java/com/sustech/sqllab/Analyzer.java
+++ backend/tpl-db-importer/src/test/java/com/sustech/sqllab/Analyzer.java
@@ -7,20 +7,21 @@ import com.sustech.sqllab.dao.VersionWithFingerprintDao;
 import com.sustech.sqllab.po.Artifact;
 import com.sustech.sqllab.po.Version;
 import com.sustech.sqllab.po.VersionWithFingerprint;
+import lombok.Cleanup;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
-import static org.springframework.util.ResourceUtils.getFile;
 
 @SuppressWarnings("NewClassNamingConvention")
 @SpringBootTest
@@ -32,112 +33,73 @@ public class Analyzer {
 	private VersionDao versionDao;
 	@Resource
 	private ArtifactDao artifactDao;
-	private static List<String> hashes;
-
-	@BeforeAll
-	static void loadHashes() throws IOException {
-		hashes = new HashSet<>(Files.readAllLines(getFile("classpath:apk.txt").toPath()))
-					.stream()
-					.map(cfg -> cfg.split(" "))
-//					.filter(cfg -> !cfg[1].equals("1"))
-					.map(cfg->cfg[0])
-					.toList();
-	}
 
 	@Test
-	void analyze(){
+	void analyzeOneApk(){
+		System.out.print(analyzeHashes("src/main/resources/result.txt"));
+	}
+
+	@SuppressWarnings("DataFlowIssue")
+	@Test
+	void analyzeApks() throws IOException {
+		for (File apk : new File("src/main/resources/apk").listFiles()) {
+			String apkPath = apk.getAbsolutePath();
+			@Cleanup FileWriter writer=new FileWriter(apkPath+"/report.txt");
+			writer.write(analyzeHashes(apkPath+"/result.txt"));
+			System.out.println(apk.getName()+" Done");
+		}
+	}
+
+	/**
+	 * @return 匹配结果
+	 */
+	@SuppressWarnings("StringConcatenationInsideStringBufferAppend")
+	@SneakyThrows(IOException.class)
+	private String analyzeHashes(String apkHashesPath){
+		List<String> apkHashes =new HashSet<>(Files.readAllLines(Path.of(apkHashesPath)))
+												.stream()
+												.map(cfg -> cfg.split(" "))
+//												.filter(cfg -> !cfg[1].equals("1"))
+												.map(cfg->cfg[0])
+												.toList();
 		List<VersionWithFingerprint> matchedEntries =
 				versionWithFingerprintDao.selectList(
-						 new LambdaQueryWrapper<VersionWithFingerprint>()
-							.in(VersionWithFingerprint::getFingerprintId, hashes));
+						new LambdaQueryWrapper<VersionWithFingerprint>()
+								.in(VersionWithFingerprint::getFingerprintId, apkHashes));
 		//versionId->weight
 		Map<Integer, Integer> matchedVersionIds = matchedEntries.stream()
 				.collect(toMap(VersionWithFingerprint::getVersionId,
-						       VersionWithFingerprint::getCount,
-						       Integer::sum));
+						VersionWithFingerprint::getCount,
+						Integer::sum));
 		List<Version> matchedVersions = versionDao.selectBatchIds(matchedVersionIds.keySet());
-		Map<Artifact, List<Version>> groupedVersions = matchedVersions.stream().collect(groupingBy(version->artifactDao.selectById(version.getArtifactId())));
+		Set<Integer> matchedArtifactIds= matchedVersions.stream()
+														.map(Version::getArtifactId)
+														.collect(toSet());
+		List<Artifact> matchedArtifacts = artifactDao.selectBatchIds(matchedArtifactIds);
+		//Version->Artifact
+		Map<Version,Artifact> artifactMap=new HashMap<>();
+		for (Version version : matchedVersions) {
+			for (Artifact artifact : matchedArtifacts) {
+				if(artifact.getId().equals(version.getArtifactId())){
+					artifactMap.put(version,artifact);
+					break;
+				}
+			}
+		}
+		Map<Artifact, List<Version>> groupedVersions = matchedVersions.stream().collect(groupingBy(artifactMap::get));
 		groupedVersions.values().forEach(vers -> {
 			Comparator<Version> comparator = comparingInt(ver->matchedVersionIds.get(ver.getId()));
 			vers.sort(comparator.reversed());
 		});
 
+		StringBuilder res = new StringBuilder();
 		for (Artifact artifact : groupedVersions.keySet()) {
-			System.out.println("\n["+artifact.getName()+"]");
+			res.append("["+artifact.getName()+"]\n");
 			for (Version version : groupedVersions.get(artifact)) {
-				System.out.println(version.getName());
+				res.append(version.getName()+"\n");
 			}
+			res.append('\n');
 		}
-	}
-
-	@Test
-	@SuppressWarnings("DataFlowIssue")
-	void matchWithoutDb() throws IOException{
-		for (File group : getFile("classpath:root").listFiles()) {
-			for (File artifact : group.listFiles()) {
-				version:for (File version : artifact.listFiles()) {
-					for (String cfg : Files.readAllLines(version.toPath())) {
-						if(hashes.contains(cfg.split(" ")[0])){
-							System.out.printf("[%s][%s][%s] matched\n",group.getName(),artifact.getName(),version.getName());
-							continue version;
-						}
-					}
-					System.out.printf("[%s][%s][%s] not matched\n",group.getName(),artifact.getName(),version.getName());
-				}
-			}
-		}
-	}
-
-	@Test
-	void testOneVersion() throws IOException{
-		Set<String> versionHashes =  new HashSet<>(Files.readAllLines(getFile("classpath:root/androidx.activity/activity-compose/1.6.0.txt").toPath()))
-										.stream()
-										.map(cfg -> cfg.split(" "))
-										.map(cfg->cfg[0])
-										.collect(toSet());
-		int count=0;
-		for (String hash : hashes) {
-			if(versionHashes.contains(hash)){
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
-
-	@Test
-	@SuppressWarnings("DataFlowIssue")
-	@SneakyThrows(IOException.class)
-	void compareMethodName(){
-//		Set<String> tplMethods = Arrays.stream(getFile("classpath:root").listFiles())
-//										.flatMap(group -> Arrays.stream(group.listFiles()))
-//										.flatMap(artifact -> Arrays.stream(artifact.listFiles()))
-//										.flatMap(version -> {
-//											try {
-//												return Files.readAllLines(version.toPath()).stream();
-//											} catch (IOException ignored) {
-//												return null;
-//											}
-//										})
-//										.collect(toSet());
-		Set<String> methods =  new HashSet<>(Files.readAllLines(getFile("classpath:methodName.txt").toPath()));
-//		methods.retainAll(tplMethods);
-		for (File group : getFile("classpath:root").listFiles()) {
-			for (File artifact : group.listFiles()) {
-				for (File version : artifact.listFiles()) {
-					for (String method : Files.readAllLines(version.toPath())) {
-						if(methods.contains(method)){
-							System.out.printf("[%s][%s][%s]\n",group.getName(),artifact.getName(),version.getName());
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@Test
-	@SneakyThrows(IOException.class)
-	void lineNum(){
-		System.out.println(Files.readAllLines(getFile("classpath:methodName.txt").toPath()).size());
+		return res.toString();
 	}
 }
